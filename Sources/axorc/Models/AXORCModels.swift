6078e0646a98e4a0c17811df7800b4c58be5b63a
Peter Steinberger <steipete@gmail.com>
2026-08-31 23:44:32 -0700

    chore(release): prepare AXorcist 0.1.9

--- Sources/axorc/Models/AXORCModels.swift
+++ Sources/axorc/Models/AXORCModels.swift
@@ -8,7 +8,7 @@ import Foundation
 
 // MARK: - Version and Configuration
 
-nonisolated let axorcVersion = "0.1.8"
+nonisolated let axorcVersion = "0.1.9"
 
 /// Returns a human-readable build stamp (yyMMddHHmm) evaluated at runtime.
 /// Good enough for confirming we're on the binary we just built.
